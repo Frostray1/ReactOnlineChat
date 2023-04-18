3ab2144f5c00c02561e5b5e386129ccedf461ad1
Artem <a.bychkoff1@yandex.ru>
2023-04-18 17:22:41 +0300

    ----

--- reactonlinechat/src/hooks/useUpdateUserDataInMessage.jsx
+++ reactonlinechat/src/hooks/useUpdateUserDataInMessage.jsx
@@ -66,19 +66,7 @@ function UpdateUserDataInChats(uid) {
         console.log(error);
       });
   });
-  // // Create an initial document to update.
-  // const frankDocRef = doc(db, "users", "frank");
-  // await setDoc(frankDocRef, {
-  //     name: "Frank",
-  //     favorites: { food: "Pizza", color: "Blue", subject: "recess" },
-  //     age: 12
-  // });
 
-  // // To update age and favorite color:
-  // await updateDoc(frankDocRef, {
-  //     "age": 13,
-  //     "favorites.color": "Red"
-  // });
 }
 
 export default UpdateUserDataInChats;
